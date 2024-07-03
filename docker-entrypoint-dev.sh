2dd61fd7d2f50c4cc81ae9e0025be811011a5f71
Alessandro Caridi <alessandro@fluidware.it>
2024-07-03 11:28:25 +0200

    docker-entrypoint-dev.sh: add version file check

--- docker-entrypoint-dev.sh
+++ docker-entrypoint-dev.sh
@@ -4,6 +4,7 @@ set -ex
 date
 
 PACKAGE_LOCK_RUN_FILE=node_modules/.package-lock.run
+VERSION_UPDATE_SCRIPT_FILE=scripts/version-update.js
 
 if [ -f $PACKAGE_LOCK_RUN_FILE ]; then
   echo "$PACKAGE_LOCK_RUN_FILE found, running npm ci"
@@ -19,6 +20,20 @@ fi
 if [ package-lock.json -nt node_modules/.package-lock.json ]; then
   echo "package-lock.json is newer than node_modules/.package-lock.json, running npm ci"
   npm ci
+fi
+
+if [ ! -f src/version/version.ts ]; then
+  echo "no version file found. starting version-update.js"
+  if [ -e "$VERSION_UPDATE_SCRIPT_FILE" ]; then
+      if grep -q '^[^#]' "$VERSION_UPDATE_SCRIPT_FILE"; then
+          echo "Updating version."
+          node scripts/version-update.js
+      else
+          echo "Nothing to do."
+      fi
+  else
+      echo "File for version update does not exist."
+  fi
 fi
 
 exec "$@"
