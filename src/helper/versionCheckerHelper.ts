fluidware-team/natuin

Initial implementation

src/helper/versionCheckerHelper.ts
@@ -1,0 +1,91 @@
+/*
+ * Copyright Fluidware srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { VERSION } from '../version';
+import { EnvParse, getLogger } from '@fluidware-it/saddlebag';
+import semver from 'semver';
+import { DbClient } from '@fluidware-it/mysql2-client';
+import { UsersCounter } from '../models/UsersCounter';
+
+const CHECK_UPDATE_URL = 'https://natuin.it/version';
+const CHECK_INTERVAL = 604800000; // 7days: 7 * 24 * 60 * 60 * 1000;
+const intervalGap = 3600000; // 1hour: 60 * 60 * 1000;
+let lastCheck = 0;
+let interval: NodeJS.Timeout;
+
+function checkUpdate() {
+  const now = Date.now();
+  if (now - lastCheck < intervalGap) {
+    return;
+  }
+  lastCheck = now;
+  const logger = getLogger().child({ component: 'versionCheckerHelper' });
+  const timeout = Math.floor(Math.random() * (60000 - 1000) + 1000);
+  logger.debug(`Checking for new version in ${Math.floor(timeout / 1000)} seconds`);
+  setTimeout(async function () {
+    const dbClient = new DbClient();
+    try {
+      await dbClient.open();
+      const users = await UsersCounter.getUsersCount(dbClient);
+      const stats = await UsersCounter.getStats(dbClient);
+      const res = await fetch(CHECK_UPDATE_URL, {
+        headers: {
+          'User-Agent': `natuin/${VERSION}`,
+          Accept: 'application/json',
+          'x-natuin-id': stats?.id,
+          'x-natuin-users': `${users}`,
+          'x-natuin-history': `${stats?.historyCount}`
+        }
+      });
+      if (!res.ok) {
+        return;
+      }
+      const data = await res.json();
+      const version = data.version;
+      const securityUpdate = !!data.security_update;
+      if (semver.gt(version, VERSION)) {
+        if (securityUpdate) {
+          logger.warn(`Security update available: ${version}`);
+        } else {
+          logger.info(`Update available: ${version}`);
+        }
+      }
+    } catch (_error) {
+      //
+    } finally {
+      try {
+        await dbClient.close();
+      } catch (e) {
+        //
+      }
+    }
+  }, timeout);
+}
+
+export function startVersionChecker() {
+  if (EnvParse.envBool('ATUIN_DISABLE_VERSION_CHECK', false)) {
+    return;
+  }
+  if (!interval) {
+    checkUpdate();
+    interval = setInterval(checkUpdate, CHECK_INTERVAL);
+    process.once('SIGTERM', () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    });
+  }
+}

src/models/Token.ts
@@ -1,0 +1,88 @@
+/*
+ * Copyright Fluidware srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { DbClient, getDateAsUTCMysqlString } from '@fluidware-it/mysql2-client';
+import { Settings } from '../Settings';
+import { generateAPIKey } from 'prefixed-api-key';
+import { ATUIN_KEY_PREFIX, PUBLIC_URL } from '../Consts';
+import { User } from '../types';
+import { HTTPError } from '@fluidware-it/express-microservice';
+
+export class Token {
+  static async createToken(dbClient: DbClient, userId: string) {
+    const key = await generateAPIKey({ keyPrefix: ATUIN_KEY_PREFIX });
+    if (!key || !key.longTokenHash || !key.shortToken) {
+      throw new Error('Failed to generate API key');
+    }
+    const expiresAt = Settings.sessionTTL
+      ? new Date(Date.now() + Settings.sessionTTL)
+      : new Date('2099-12-31T23:59:59.999Z');
+    await dbClient.insert('insert into tokens (tokenHash, shortToken, userId, expiresAt) values (?, ?, ?, ?)', [
+      key.longTokenHash,
+      key.shortToken,
+      userId,
+      getDateAsUTCMysqlString(expiresAt)
+    ]);
+    return key.token;
+  }
+  static async getUserByToken(dbClient: DbClient, shortToken: string, tokenHash: string): Promise<User | undefined> {
+    const row = await dbClient.get(
+      `select u.userId, u.email, u.username, u.blocked, u.validatedAt, u.admin, u.createdAt
+            from
+                tokens t, 
+                users u
+            where
+                t.userId = u.userId and
+                t.tokenHash = ? and 
+                t.shortToken = ? and 
+                t.expiresAt > now()`,
+      [tokenHash, shortToken]
+    );
+    if (row) {
+      if (row.blocked) {
+        throw new HTTPError('user blocked. please contact instance administrator', 403);
+      }
+      if (!row.validatedAt) {
+        const now = Date.now();
+        const createdAt = row.createdAt as Date;
+        const diff = now - createdAt.getTime();
+        if (diff > Settings.emailRegistrationValidationTimeout) {
+          throw new HTTPError(
+            `user not validated. please reset your password to validate it ${PUBLIC_URL}/account/forgot-password`,
+            403
+          );
+        }
+      }
+      await dbClient.run('update tokens set lastUsedAt = current_timestamp(3) where shortToken = ?', [shortToken]);
+      return {
+        id: row.userId,
+        username: row.username,
+        email: row.email,
+        validated: !!row.validatedAt,
+        isAdmin: !!row.admin
+      };
+    }
+  }
+  static async deleteToken(dbClient: DbClient, userId: string, shortToken: string) {
+    return dbClient.delete('delete from tokens where shortToken = ? and userId = ?', [shortToken, userId]);
+  }
+  static async getTokens(dbClient: DbClient, userId: string) {
+    return dbClient.all(
+      'select shortToken, expiresAt, lastUsedAt from tokens where userId = ? order by createdAt asc',
+      [userId]
+    );
+  }
+}

fluidware-team/atuin-node-server

src/helper/versionCheckerHelper.ts
@@ -1,0 +1,91 @@
+/*
+ * Copyright Fluidware srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { VERSION } from '../version';
+import { EnvParse, getLogger } from '@fluidware-it/saddlebag';
+import semver from 'semver';
+import { DbClient } from '@fluidware-it/mysql2-client';
+import { UsersCounter } from '../models/UsersCounter';
+
+const CHECK_UPDATE_URL = 'https://update.natuin.fluidware.it/version';
+const CHECK_INTERVAL = 604800000; // 7days: 7 * 24 * 60 * 60 * 1000;
+const intervalGap = 3600000; // 1hour: 60 * 60 * 1000;
+let lastCheck = 0;
+let interval: NodeJS.Timeout;
+
+function checkUpdate() {
+  const now = Date.now();
+  if (now - lastCheck < intervalGap) {
+    return;
+  }
+  lastCheck = now;
+  const logger = getLogger().child({ component: 'versionCheckerHelper' });
+  const timeout = Math.floor(Math.random() * (60000 - 1000) + 1000);
+  logger.debug(`Checking for new version in ${Math.floor(timeout / 1000)} seconds`);
+  setTimeout(async function () {
+    const dbClient = new DbClient();
+    try {
+      await dbClient.open();
+      const users = await UsersCounter.getUsersCount(dbClient);
+      const stats = await UsersCounter.getStats(dbClient);
+      const res = await fetch(CHECK_UPDATE_URL, {
+        headers: {
+          'User-Agent': `natuin/${VERSION}`,
+          Accept: 'application/json',
+          'x-natuin-id': stats?.id,
+          'x-natuin-users': `${users}`,
+          'x-natuin-history': `${stats?.historyCount}`
+        }
+      });
+      if (!res.ok) {
+        return;
+      }
+      const data = await res.json();
+      const version = data.version;
+      const securityUpdate = !!data.security_update;
+      if (semver.gt(version, VERSION)) {
+        if (securityUpdate) {
+          logger.warn(`Security update available: ${version}`);
+        } else {
+          logger.info(`Update available: ${version}`);
+        }
+      }
+    } catch (_error) {
+      //
+    } finally {
+      try {
+        await dbClient.close();
+      } catch (_error2) {
+        //
+      }
+    }
+  }, timeout);
+}
+
+export function startVersionChecker() {
+  if (EnvParse.envBool('NATUIN_DISABLE_VERSION_CHECK', false)) {
+    return;
+  }
+  if (!interval) {
+    checkUpdate();
+    interval = setInterval(checkUpdate, CHECK_INTERVAL);
+    process.once('SIGTERM', () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    });
+  }
+}

src/models/Token.ts
@@ -1,0 +1,88 @@
+/*
+ * Copyright Fluidware srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { DbClient, getDateAsUTCMysqlString } from '@fluidware-it/mysql2-client';
+import { Settings } from '../Settings';
+import { generateAPIKey } from 'prefixed-api-key';
+import { NATUIN_KEY_PREFIX, PUBLIC_URL } from '../consts';
+import { User } from '../types';
+import { HTTPError } from '@fluidware-it/express-microservice';
+
+export class Token {
+  static async createToken(dbClient: DbClient, userId: string) {
+    const key = await generateAPIKey({ keyPrefix: NATUIN_KEY_PREFIX });
+    if (!key || !key.longTokenHash || !key.shortToken) {
+      throw new Error('Failed to generate API key');
+    }
+    const expiresAt = Settings.sessionTTL
+      ? new Date(Date.now() + Settings.sessionTTL)
+      : new Date('2099-12-31T23:59:59.999Z');
+    await dbClient.insert('insert into tokens (tokenHash, shortToken, userId, expiresAt) values (?, ?, ?, ?)', [
+      key.longTokenHash,
+      key.shortToken,
+      userId,
+      getDateAsUTCMysqlString(expiresAt)
+    ]);
+    return key.token;
+  }
+  static async getUserByToken(dbClient: DbClient, shortToken: string, tokenHash: string): Promise<User | undefined> {
+    const row = await dbClient.get(
+      `select u.userId, u.email, u.username, u.blocked, u.validatedAt, u.admin, u.createdAt
+            from
+                tokens t,
+                users u
+            where
+                t.userId = u.userId and
+                t.tokenHash = ? and
+                t.shortToken = ? and
+                t.expiresAt > now()`,
+      [tokenHash, shortToken]
+    );
+    if (row) {
+      if (row.blocked) {
+        throw new HTTPError('user blocked. please contact instance administrator', 403);
+      }
+      if (!row.validatedAt) {
+        const now = Date.now();
+        const createdAt = row.createdAt as Date;
+        const diff = now - createdAt.getTime();
+        if (diff > Settings.emailRegistrationValidationTimeout) {
+          throw new HTTPError(
+            `user not validated. please reset your password to validate it ${PUBLIC_URL}/account/forgot-password`,
+            403
+          );
+        }
+      }
+      await dbClient.run('update tokens set lastUsedAt = current_timestamp(3) where shortToken = ?', [shortToken]);
+      return {
+        id: row.userId,
+        username: row.username,
+        email: row.email,
+        validated: !!row.validatedAt,
+        isAdmin: !!row.admin
+      };
+    }
+  }
+  static async deleteToken(dbClient: DbClient, userId: string, shortToken: string) {
+    return dbClient.delete('delete from tokens where shortToken = ? and userId = ?', [shortToken, userId]);
+  }
+  static async getTokens(dbClient: DbClient, userId: string) {
+    return dbClient.all(
+      'select shortToken, expiresAt, lastUsedAt from tokens where userId = ? order by createdAt asc',
+      [userId]
+    );
+  }
+}